a291a74a8fadda497c06729d4985894f0c0f2a86
renuevo <novelty0110@gmail.com>
2019-09-16 18:05:33 +0900

    plugin setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    'build-scan'
+    `maven-publish`
     kotlin("jvm") version "1.3.50"
-    "java-library"
 }
 
 group = "com.github"
@@ -12,12 +13,31 @@ repositories {
     mavenCentral()
 }
 
+buildScan{
+    termsOfServiceAgree = "yes"
+    publishAlways()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.9")
     implementation("net.sf.opencsv:opencsv:2.3")
-    testCompile ("junit:junit:4.12")
+    testImplementation ("junit:junit:4.12")
 }
 
 tasks.withType<KotlinCompile> {
